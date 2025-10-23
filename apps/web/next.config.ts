ecdf4f12ce0b59332644ae465ec2270a9d9825f6
Tămaș Cosmin <70478656+CsmnTms@users.noreply.github.com>
2025-10-24 01:32:40 +0300

    update pages.yml

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
-const isProjectPage = false; // set true if deploying to https://<user>.github.io/<repo>
+const isProjectPage = true; // set true if deploying to https://<user>.github.io/<repo>
 const repo = 'showcase';    // your repo name
 
 const nextConfig: NextConfig = {
   output: 'export', // enables static export
   images: { unoptimized: true }, // required for next/image on static hosts
   basePath: isProjectPage ? `/${repo}` : undefined,
-  assetPrefix: isProjectPage ? `/${repo}/` : undefined, // comment for local testing
+  assetPrefix: isProjectPage ? `/${repo}/` : undefined,
   trailingSlash: true, // safer for GH Pages (serves index.html in folders)
   reactCompiler: true,
 };
